18af41a20c174e09bececaacc17550df2d481d2f
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-31 17:18:20 -0300

    apostila02: bloco de codigo 3.3.5
    exercicio envolvendo percepcao humana de calor proposto

--- index.js
+++ index.js
@@ -11,4 +11,32 @@ const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
 const url = `${base_url}?appid=${appid}&q=${q}&units=${units}&lang=${lang}&cnt=${cnt}`
 
-console.log(appid)
+const result = axios.get(url)
+
+result
+.then(res => {
+  //console.log(res)
+  return res.data
+})
+.then(res => {
+  //console.log(res.data)
+  return res['list']
+})
+.then(res => {
+  for (previsao of res){
+    const dt = previsao.dt
+    const temp_min = previsao.main.temp_min
+    const temp_max = previsao.main.temp_max
+    const humidity = previsao.main.humidity
+    const description = previsao.weather[0].description
+    console.log(`${new Date(+dt * 1000).toLocaleTimeString()}`)
+    console.log(`Temp min: ${temp_min}\u00B0C`)
+    console.log(`Temp max: ${temp_max}\u00B0C`)
+    console.log(`Humidity: ${humidity}%`)
+    console.log(`Description: ${description}`)
+  }
+  return res
+})
+.then (res => {
+  //mostre quantas previsões têm percepção humana de calor acima de 15 celsius
+})
